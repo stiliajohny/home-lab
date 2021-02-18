022d6d063bd09b5aff2cc8cc447b9cef5a2e5244
John Stilia <stilia.johny@gmail.com>
2021-02-18 00:25:54 +0000

    prep upload

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "pm_api_url" {
+  default = "https://192.168.1.22:8006/api2/json"
+}
+
+variable "pm_user" {
+  default = "terraform-prov@pve"
+}
+
+variable "pm_password" {
+  default = "passwordpasswrod"
+}
+
+variable "ssh_key" {
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDVWIuggvI+DppU7AElJIBMoi4ydjdlyBixcnDvzSCY15W2PPRKvznbsScEzkc6wkJKHoQRmS3+SIRg3KFO9I6Pazj2eF5pJCFEgzubjyOEkywQMW68O3qW5xmjLYec40ENKrBYv2NTaqjkwkd0mPUcxE8SD5p+g8ZrU21slxgU+u6nQXT5lx2wTOQg8W4ii7rHywRli6RBfvHPKbOKh4SZeYkgvUN3fmL06vn+/KT0rBHZ8llFHB7nuhJK5cIYS0rhovdYw8FZicFS/7YcjaY0SY0w78KOYRToP2qZ9I+uzjwCF7/V9fDT11sDL6YHCuwOjSKV/WGbUd55qNPnc4W3dlsFxQbTyKCFGzuR6ib49LteMdAhMYVGgyxR/vyYV1xPyCL0lI0EJl086jNwC46friQ/rSeE66UgRY/UPgxOhUlCMcz4v/ubb4qol8a0p5Gn8ft+1hPIlWfZ+LHKFJ4HeEmRp+IJoV/WU0yJ5FzylWRXQMsL1OW6LsTz3eiEFzE= jstilia@dell-xps-7590-archlabs-laptop.johnstilia.internal"
+}
